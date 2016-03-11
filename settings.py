de0df1369951fae3d73e29295b28cc55fa6f4066
Kirill <kirrax@gmail.com>
2016-03-11 21:31:08 +0200

    initial

---
+++ settings.py
@@ -0,0 +1,14 @@
+
+GAME_SPEED = 2.5  # fps, the more the faster
+
+MAX_PLAYERS = 10
+NUM_COLORS = 7  # set according to the number of css classes
+
+FIELD_SIZE_X = 50  # game field size in characters
+FIELD_SIZE_Y = 25
+
+INIT_LENGHT = 3
+
+DIGIT_SPAWN_RATE = 5  # probability to spawn per frame in %
+STONE_SPAWN_RATE = 5  # note that digit spawn calculated for every snake
+                      # while stone spawn is calculated once per frame
